8e3203d4715b60b4fa0a97b227ce6a1ed1c244d9
Andres Castro <andres435b@gmail.com>
2022-03-16 14:14:08 -0700

    the program does 1 iteration of the problem this version is finished i have to create a branch to start working on a better version

--- operations-research-calculator/main.cpp
+++ operations-research-calculator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ int main()
 
     int numberOfColumns = numberOfDesicionVariables + numberOfHolguras +1;
     int numberOfRows = numberOfHolguras +1;
-    int simplexTable[numberOfColumns * numberOfRows];
+    double simplexTable[numberOfColumns * numberOfRows];
 
 
     cout << "Write the table values: " << endl;
@@ -57,6 +58,76 @@ int main()
     for(int i = 0; i < numberOfColumns * numberOfRows; i++)
     {
         cout << simplexTable[i] << " | ";
+        if((i+1)%numberOfColumns == 0)
+            cout << endl;
+    }
+
+    double* ptr_mostNegativeColumn = &simplexTable[0];
+    for(int variableIndex = 1; variableIndex < numberOfDesicionVariables; variableIndex++)
+    {
+        if(simplexTable[variableIndex] < *ptr_mostNegativeColumn)
+            ptr_mostNegativeColumn = &simplexTable[variableIndex];
+    }
+
+    double* ptr_pivotRow = &simplexTable[numberOfColumns*2 -1];
+    double smallestSolutionRate = *ptr_pivotRow / *(ptr_mostNegativeColumn + numberOfColumns);
+    double pivotValue = *(ptr_mostNegativeColumn + numberOfColumns);
+    for(int currentRow = 3; currentRow <= numberOfRows; currentRow++)
+    {
+        double currentPivotColumnValue = *(ptr_mostNegativeColumn + numberOfColumns*(currentRow -1));
+        int solutionIndex = numberOfColumns*currentRow -1;
+        if(currentPivotColumnValue > 0 && simplexTable[solutionIndex] > 0)
+        {
+            double currentSolutionRate = simplexTable[solutionIndex] / currentPivotColumnValue;
+            if(currentSolutionRate < smallestSolutionRate)
+            {
+                ptr_pivotRow = &simplexTable[solutionIndex];
+                smallestSolutionRate = currentSolutionRate;
+                pivotValue = currentPivotColumnValue;
+            }
+        }
+    }
+    ptr_pivotRow -= (numberOfColumns -1);
+    cout << endl << "_________________________________________________________________________________" << endl;
+    cout << "Most negative column value: " << *ptr_mostNegativeColumn << endl;
+    cout << "pivot row first value: " << *ptr_pivotRow << endl;
+    cout << "Pivot value: " << pivotValue << endl;
+
+    for(int rowIndex = -1; rowIndex < numberOfRows; rowIndex++)
+    {
+        cout << "iterated trhough" << endl;
+        if(rowIndex != -1 && &simplexTable[rowIndex * numberOfColumns] != ptr_pivotRow)
+        {
+            double pivotColumnValue = *(ptr_mostNegativeColumn + rowIndex * numberOfColumns);
+            for(int columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
+            {
+                if(pivotColumnValue < 0)
+                    simplexTable[columnIndex + rowIndex * numberOfColumns] -= ( pivotColumnValue * (*(ptr_pivotRow + columnIndex)));
+                else
+                    simplexTable[columnIndex + rowIndex * numberOfColumns] -= ( pivotColumnValue * (*(ptr_pivotRow + columnIndex)));
+            }
+        }
+        else if(rowIndex == -1)
+        {
+            cout << "ptr_PivotRow: " << ptr_pivotRow << endl;
+            cout << "*ptr_PivotRow: " << *ptr_pivotRow << endl;
+            for(int columnIndex = 0; columnIndex < numberOfColumns; columnIndex++)
+            {
+                *ptr_pivotRow = *ptr_pivotRow / pivotValue;
+                ptr_pivotRow++;
+            }
+            ptr_pivotRow -= numberOfColumns;
+            cout << "ptr_PivotRow: " << ptr_pivotRow << endl;
+            int num;
+            cin >> num;
+        }
+    }
+
+    cout << endl << "_________________________________________________________________________________" << endl;
+    cout << endl << "Final simplex Table: " << endl;
+    for(int i = 0; i < numberOfColumns * numberOfRows; i++)
+    {
+        printf("%f | ", simplexTable[i]);
         if((i+1)%numberOfColumns == 0)
             cout << endl;
     }
